41baf84b9007dbbe96f6232f68f3190e3f324072
szonjao <szonja.o@me.com>
2015-08-12 19:04:14 +0200

    cool

--- js/app.js
+++ js/app.js
@@ -24,6 +24,19 @@ $(document).ready(function(){
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
+	$(document).keydown(function(e){
+		if (e.which === 88) {
+			/*$('.ryu-still').hide();
+			$('.ryu-ready').hide();*/
+			$('.ryu-throwing').hide();
+			$('.ryu-cool').show();
+		}
+	}).keyup(function(e){
+		if (e.which === 88) {
+			$('.ryu-cool').hide();
+			/*$('.ryu-still').show();*/
+		}
+	});
 });
 function playHadouken(){
 	$('#hadouken-sound')[0].volume=0.5;
